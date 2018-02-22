5d36cc2d36153aca67e2436549215c7cae7aebe3
Susan Lippa <sfl@dhcp-10-102-159-187.wireless.northwestern.private>
2018-02-22 15:30:21 -0600

    removed cosolelogs

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,17 +1,6 @@
 //LOAD DATA
 var totalDiff;
 var friends = require("../data/friends.js");
-//console.log("friends", friends);
-// for (key in friends) {
-//   console.log(key);
-//   console.log("name", friends[key].name);
-//   console.log("image", friends[key].image);
-//   console.log("scores", friends[key].scores);
-//   for (i = 0; i < friends[key].scores.length; i++) {
-//     console.log("eachScore", (parseInt(friends[key].scores[i])));
-//    }
-// };
-
 
 
 module.exports = function(app){
@@ -36,20 +25,12 @@ module.exports = function(app){
 
   app.post("/api/friends", function(req, res) {
     var userInfo = req.body;
-      console.log("userInfo", userInfo);
-      console.log("scores", userInfo['scores[]']);
     for (key in friends) {
       totalDiff = 0;
-      console.log("key", key);
       for (i = 0; i < userInfo['scores[]'].length; i++) {
-        console.log("eachScoreUI", (parseInt(userInfo['scores[]'][i])));
-        console.log("eachScoreFr", (parseInt(friends[key].scores[i])));
       var eachScoreDiff =  Math.abs((parseInt(userInfo['scores[]'][i])) - (parseInt(friends[key].scores[i])));
-        console.log("eachScoreDiff", eachScoreDiff);
       totalDiff = totalDiff + eachScoreDiff;
-        console.log("totalDiffIn", totalDiff);
       }
-        console.log("totalDiffOut", totalDiff);
       friends[key].netScore = totalDiff;
     };
 
